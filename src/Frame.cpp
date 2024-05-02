7c6119918d19b89a8dd577d414e9773278ce7561
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-05-02 08:30:10 +0300

    Updated the documentation.

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -5,38 +5,78 @@
 #include "Frame.h"
 #include <algorithm>
 
+/**
+ * Constructor of Frame class which takes inputStream as input and reads the frame
+ *
+ * @param name  Name of the frame
+ */
 Frame::Frame(const string& _name) {
     this->name = _name;
 }
 
+/**
+ * Checks if the given lexical unit exists in the current frame
+ * @param lexicalUnit Lexical unit to be searched.
+ * @return True if the lexical unit exists, false otherwise.
+ */
 bool Frame::lexicalUnitExists(const string& synSetId) const{
     return find(lexicalUnits.begin(), lexicalUnits.end(), synSetId) != lexicalUnits.end();
 }
 
+/**
+ * Accessor for a given index in the lexicalUnit array.
+ * @param index Index of the lexical unit
+ * @return The lexical unit at position index in the lexicalUnit array
+ */
 string Frame::getLexicalUnit(int index) const{
     return lexicalUnits.at(index);
 }
 
+/**
+ * Accessor for a given index in the frameElements array.
+ * @param index Index of the frame element
+ * @return The frame element at position index in the frameElements array
+ */
 string Frame::getFrameElement(int index) const{
     return frameElements.at(index);
 }
 
+/**
+ * Returns number of lexical units in the current frame
+ * @return Number of lexical units in the current frame
+ */
 int Frame::lexicalUnitSize() const{
     return lexicalUnits.size();
 }
 
+/**
+ * Returns number of frame elements in the current frame
+ * @return Number of frame elements in the current frame
+ */
 int Frame::frameElementSize() const{
     return frameElements.size();
 }
 
+/**
+ * Accessor for the name of the frame
+ * @return Name of the frame
+ */
 string Frame::getName() const{
     return name;
 }
 
+/**
+ * Adds a new lexical unit to the current frame
+ * @param lexicalUnit Lexical unit to be added
+ */
 void Frame::addLexicalUnit(const string& lexicalUnit) {
     lexicalUnits.emplace_back(lexicalUnit);
 }
 
+/**
+ * Adds a new frame element to the current frame
+ * @param frameElement Frame element to be added
+ */
 void Frame::addFrameElement(const string& frameElement) {
     frameElements.emplace_back(frameElement);
 }
--- src/FrameNet.cpp
+++ src/FrameNet.cpp
@@ -4,6 +4,10 @@
 
 #include "FrameNet.h"
 
+/**
+ * A constructor of FrameNet class which reads all frame files inside the files2.txt file. For each
+ * filename inside that file, the constructor creates a FrameNet.Frame and puts in inside the frames {@link ArrayList}.
+ */
 FrameNet::FrameNet() {
     XmlDocument xmlDocument = XmlDocument("framenet.xml");
     xmlDocument.parse();
@@ -28,6 +32,11 @@ FrameNet::FrameNet() {
     }
 }
 
+/**
+ * Checks if the given lexical unit exists in any frame in the frame set.
+ * @param synSetId Id of the lexical unit
+ * @return True if any frame contains the given lexical unit, false otherwise.
+ */
 bool FrameNet::lexicalUnitExists(const string& synSetId) const{
     for (const Frame& frame : frames){
         if (frame.lexicalUnitExists(synSetId)){
@@ -37,6 +46,11 @@ bool FrameNet::lexicalUnitExists(const string& synSetId) const{
     return false;
 }
 
+/**
+ * Returns an array of frames that contain the given lexical unit in their lexical units
+ * @param synSetId Id of the lexical unit.
+ * @return An array of frames that contains the given lexical unit.
+ */
 vector<Frame> FrameNet::getFrames(const string& synSetId) const{
     vector<Frame> result;
     for (const Frame& frame : frames){
@@ -47,10 +61,19 @@ vector<Frame> FrameNet::getFrames(const string& synSetId) const{
     return result;
 }
 
+/**
+ * Returns number of frames in the frame set.
+ * @return Number of frames in the frame set.
+ */
 int FrameNet::size() const{
     return frames.size();
 }
 
+/**
+ * Returns the element at the specified position in the frame list.
+ * @param index index of the element to return
+ * @return The element at the specified position in the frame list.
+ */
 Frame FrameNet::getFrame(int index) const{
     return frames.at(index);
 }
